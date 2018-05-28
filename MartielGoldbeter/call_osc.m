e66c58e0a172b6002a0285d436f540c52184bec0
ChuqiaoH <cqhuyan@bu.edu>
2018-05-28 19:36:11 -0400

    Update call_osc.m
    Added simple code to call three_variable_fun!

--- MartielGoldbeter/call_osc.m
+++ MartielGoldbeter/call_osc.m
@@ -55,3 +55,31 @@ ylabel('\alpha, \beta/60, \gamma/60')
 xlabel('Time (min)')
 legend({'\alpha','\beta','\gamma','\rho_{T}'})
 set(gca,'fontsize',18)
+
+%%%%% Three variable model with parameter input%%%%%
+% Some parameters based on Table 2
+alpha=3;% intracellular ATP level 
+sigma=0.6; 
+q=4000; 
+ki=1.7; 
+kt=0.9;
+h=5; 
+ke=5.4; 
+k1=0.036; 
+k2=0.666; 
+L1=10;
+L2=0.005; 
+c=10; 
+theta=0.01; % not available from experimental data
+lambda=0.01;% not available from experimental data
+eps=1; % not available from experimental data
+
+x0=[0.7 0 0];
+tspan=[0 80];
+
+[t1,x1]=ode45(@(t,x) three_variable_fun(t,x,alpha,sigma,q,ki,kt,h,ke,k1,k2,L1,L2,c,theta,lambda,eps),tspan,x0);
+
+% plot figure 2a
+plot(t1,x1(:,1),'k',t1,x1(:,2),'r',t1,x1(:,3),'b')
+title('Three variable simulation')
+legend('rho_T: total fraction of active receptor','[cAMP]in/K_R','[cAMP]ex/K_R')
