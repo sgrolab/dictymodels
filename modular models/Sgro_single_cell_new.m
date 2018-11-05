15bfdee516b67b4e23dec1cc5f155cd4a4325c69
ChuqiaoH <cqhuyan@bu.edu>
2018-11-05 11:39:12 -0500

    Create Sgro_single_cell_new.m
    ODE functions for single cell  in Sgro 2015 .

---
+++ modular models/Sgro_single_cell_new.m
@@ -0,0 +1,19 @@
+function dxdt=Sgro_single_cell_new(x,param,stim)
+
+e=param.e; %tauA/tauR; %episilon
+tauA=param.tauA;
+tauR=param.tauR;
+g=param.g; %gamma
+c0=param.c0;
+a=param.a;
+alpha0=param.alpha0;
+alpha_pde= param.alpha_pde;
+Kd=param.Kd;
+S=param.S;                          
+
+dxdt(1,1) = (x(1)- x(1)^3/3 - x(2) +a*log(1 + stim/Kd) ) ;
+
+% disp(strcat('dAdt=',num2str(dxdt(1))))
+dxdt(2,1)= (x(1)-g*x(2) + c0).*e./tauR;
+
+end
